362c65beb92e8512114f3b922218afc3f6677108
Jawad Ahmed <jawad.w.ahmed@gmail.com>
2022-12-04 11:33:49 +0000

    day_2_rock_paper_scissors

--- day_2_rock_paper_scissors/day_2_rock_paper_scissors/bin/main.ml
+++ day_2_rock_paper_scissors/day_2_rock_paper_scissors/bin/main.ml
@@ -1,31 +1,26 @@
 open Batteries
 
-type shape = Rock | Paper | Scissors
+let get_scores contents =
+  String.split_on_char '\n' contents
+  |> List.map (fun str ->
+         ( Char.code (String.get str 0) - Char.code 'A',
+           Char.code (String.get str 2) - Char.code 'X' ))
 
-let shape_of_string = function
-  | "A" | "X" -> Rock
-  | "B" | "Y" -> Paper
-  | "C" | "Z" -> Scissors
-  | _ -> assert false
-
-let play (opp, you) =
-  (match you with Rock -> 1 | Paper -> 2 | Scissors -> 3)
-  +
-  match (opp, you) with
-  | Scissors, Rock | Paper, Scissors | Rock, Paper -> 6
-  | Rock, Scissors | Scissors, Paper | Paper, Rock -> 0
-  | _, _ -> 3
-
-let part_one contents =
-  let round = String.split_on_char '\n' contents in
-  List.map (Tuple2.mapn shape_of_string % String.split ~by:" ") round
-  |> List.map play |> List.sum
+let part_one scores =
+  List.map (fun (opp, you) -> ((you - opp + 4) mod 3 * 3) + you + 1) scores
+  |> List.sum
   |> Printf.printf "Your score: %d\n"
+
+and part_two scores =
+  List.map (fun (opp, you) -> ((you + opp + 2) mod 3) + (3 * you) + 1) scores
+  |> List.sum
+  |> Printf.printf "Total score: %d\n"
 
 let arg = try Some (Array.get Sys.argv 1) with Invalid_argument _ -> None
 
-let () = let contents = File.with_file_in "input.txt" IO.read_all in
-match arg with
-| Some ("one"|"1") -> part_one contents
-(* | Some ("two"|"2") -> part_two contents *)
-| _ -> print_endline "Usage: day_2_rock_paper_scissors <part>"
+let () =
+  let scores = File.with_file_in "input.txt" IO.read_all |> get_scores in
+  match arg with
+  | Some ("one" | "1") -> part_one scores
+  | Some ("two" | "2") -> part_two scores
+  | _ -> print_endline "Usage: day_2_rock_paper_scissors <part>"
