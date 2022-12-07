f96722b7c54b024b99434e94f7d105ade07619f7
Jawad Ahmed <jawad.w.ahmed@gmail.com>
2022-12-07 00:00:13 +0000

    day_3_rucksack_reorganization

--- day_3_rucksack_reorganization/day_3_rucksack_reorganization/bin/main.ml
+++ day_3_rucksack_reorganization/day_3_rucksack_reorganization/bin/main.ml
@@ -1,25 +1,40 @@
 open Batteries
 
-let get_rucksacks contents =
+let priority ch =
+  if Char.is_lowercase ch then Char.code ch - Char.code 'a' + 1
+  else Char.code ch - Char.code 'A' + 27
+
+let rec part_one contents =
+  get_rucksacks contents
+  |> List.map (fun group ->
+         let first, second = Tuple2.mapn (Set.of_seq % String.to_seq) group in
+         let result = Set.intersect first second |> Set.map priority in
+         Set.fold ( + ) result 0)
+  |> List.sum |> Printf.printf "Sum: %d\n"
+
+and get_rucksacks contents =
   String.split_on_char '\n' contents
   |> List.map (fun str ->
          let mid = Int.div (String.length str) 2 in
          (String.sub str 0 mid, String.sub str mid mid))
 
-let rec part_one contents =
-  get_rucksacks contents
-  |> List.map (fun (first, second) ->
-         let first = String.to_seq first |> Set.of_seq in
-         let second = String.to_seq second |> Set.of_seq in
-         let result = Set.intersect first second |> Set.map priority in
-         Set.fold ( + ) result 0)
-  |> List.sum |> Printf.printf "Sum: %d\n"
+let rec part_two contents =
+  String.split_on_char '\n' contents
+  |> next_three
+  |> List.map (fun group ->
+         let first, second, third =
+           Tuple3.mapn (Set.of_seq % String.to_seq) group
+         in
+         Set.intersect first second |> Set.intersect third |> Set.any)
+  |> List.map priority |> List.sum |> Printf.printf "Sum: %d\n"
 
-and priority ch =
-  if Char.is_lowercase ch then Char.code ch - Char.code 'a' + 1
-  else Char.code ch - Char.code 'A' + 27
+and next_three rucksacks =
+  match rucksacks with
+  | first :: second :: third :: rest ->
+      List.cons (first, second, third) (next_three rest)
+  | [] -> []
+  | _ -> assert false
 
-let part_two _ = ()
 let arg = try Some Sys.argv.(1) with Invalid_argument _ -> None
 
 let () =
